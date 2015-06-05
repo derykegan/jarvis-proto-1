20d1e20128769e8d71b9257af11872ce857863fe
Deryk Egan <deryk.egan@strath.ac.uk>
2015-06-05 18:51:34 +0100

    Updates to EnrollmentDialog.

--- src/com/system/display/EnrollmentDialog.java
+++ src/com/system/display/EnrollmentDialog.java
@@ -2,23 +2,37 @@ package com.system.display;
 
 import javax.swing.*;
 
-import java.awt.event.*;
 import java.util.ArrayList;
 
 public class EnrollmentDialog{
 	
-	public boolean REGISTRATION_COMPLETED = false;
+	private boolean REGISTRATION_COMPLETED = false,
+			EXISTING_USERS = false;
 	private ArrayList<String> existingUserNames;
 	
 	String 	name = null,
 			nick = null,
 			gender = null;
 	
+	/**
+	 * Enrollment action.
+	 */
 	public void doEnrollment(){
 		
 		// if no users exist already, display first time message.
 		if(existingUserNames == null ||
 				existingUserNames.size() < 1){
+			
+			// set flag to save future checks
+			EXISTING_USERS = false;
+		}
+		else{
+			EXISTING_USERS = true;
+		}
+		
+		// if no users exist already, display first time message.
+		if(!EXISTING_USERS){
+			
 			JOptionPane.showMessageDialog(null, 
 					"No users have been entered," + '\n' + 
 					"please press OK to set up a" + '\n' +
@@ -27,16 +41,16 @@ public class EnrollmentDialog{
 		}
 		
 		boolean alreadyExists = false;
-		while(name == null){
+		while(name == null || EXISTING_USERS){
 			if(alreadyExists){
 				name = JOptionPane.showInputDialog(null, 
 						"Username already exists." + '\n' + '\n' +
-						"Enter a user name : ", 
+						"Enter a user name (required): ", 
 						"User Registration - Username - (1/3)", 1);
 				alreadyExists = false;
 			}
 			else{
-				name = JOptionPane.showInputDialog(null, "Enter a user name : ", 
+				name = JOptionPane.showInputDialog(null, "Enter a user name (required): ", 
 					"User Registration - Username - (1/3)", 1);
 			}
 			if(name == null){
@@ -50,15 +64,15 @@ public class EnrollmentDialog{
 				}
 			}
 		}
-		while(nick == null){
-			nick = JOptionPane.showInputDialog(null, "Enter a nickname : ", 
+		while(nick == null || EXISTING_USERS){
+			nick = JOptionPane.showInputDialog(null, "Enter a nickname (required): ", 
 					"User Registration - Nickname - (2/3)", 1);
 		}
-		while(gender == null){
+		while(gender == null || EXISTING_USERS){
 			Object[] possibilities = {"male", "female"};
 			gender = (String)JOptionPane.showInputDialog(
 			                    null,
-			                    "User Gender:",
+			                    "User Gender (required):",
 			                    "User Registration - Gender - (3/3)",
 			                    JOptionPane.PLAIN_MESSAGE,
 			                    null,
@@ -74,7 +88,9 @@ public class EnrollmentDialog{
 				"Gender: " + gender,
 				"Complete Registration", 1);
 		
-		REGISTRATION_COMPLETED = true;
+		if(name != null && nick != null && gender != null){
+			REGISTRATION_COMPLETED = true;
+		}
 	}
 	
 	/**
